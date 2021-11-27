531f361d313319913555f80b1e97ac638794b6de
James Goodman <43926484+SlothInABox@users.noreply.github.com>
2021-11-27 13:08:29 +0000

    Implemented events with ImGui
    - Can now interact with the ImGui window

--- Quark/src/Quark/Events/KeyEvent.h
+++ Quark/src/Quark/Events/KeyEvent.h
@@ -51,4 +51,19 @@ namespace Quark
 		EVENT_CLASS_TYPE(KeyReleased)
 	};
 
+	class QUARK_API KeyTypedEvent : public KeyEvent
+	{
+	public:
+		KeyTypedEvent(int keycode) : KeyEvent(keycode) {}
+
+		std::string ToString() const override
+		{
+			std::stringstream ss;
+			ss << "KeyTypedEvent: " << m_KeyCode;
+			return ss.str();
+		}
+
+		EVENT_CLASS_TYPE(KeyTyped)
+	};
+
 }
--- Quark/src/Quark/ImGui/ImGuiLayer.h
+++ Quark/src/Quark/ImGui/ImGuiLayer.h
@@ -2,6 +2,10 @@
 
 #include "Quark/Layer.h"
 
+#include "Quark/Events/ApplicationEvent.h"
+#include "Quark/Events/KeyEvent.h"
+#include "Quark/Events/MouseEvent.h"
+
 namespace Quark {
 
 	class QUARK_API ImGuiLayer : public Layer
@@ -15,6 +19,15 @@ namespace Quark {
 
 		void OnUpdate();
 		void OnEvent(Event& event);
+	private:
+		bool OnMouseButtonPressedEvent(MouseButtonPressedEvent& e);
+		bool OnMouseButtonReleasedEvent(MouseButtonReleasedEvent& e);
+		bool OnMouseMovedEvent(MouseMovedEvent& e);
+		bool OnMouseScrolledEvent(MouseScrolledEvent& e);
+		bool OnKeyPressedEvent(KeyPressedEvent& e);
+		bool OnKeyReleasedEvent(KeyReleasedEvent& e);
+		bool OnKeyTypedEvent(KeyTypedEvent& e);
+		bool OnWindowResizeEvent(WindowResizeEvent& e);
 	private:
 		float m_Time = 0.0f;
 	};
